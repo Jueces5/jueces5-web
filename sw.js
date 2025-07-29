935e3b96486a8f93e8ae210a1c14bf7d86a0356d
Jueces5 <Jueces5000@gmail.com>
2025-07-29 13:23:44 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,45 @@
-const CACHE_NAME = 'jueces5-v3';
-const ASSETS = [
-  '/',
-  '/estilos.min.css',
-  '/imagenes/logo.webp',
-  'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Open+Sans:wght@400;600&display=swap'
+const CACHE_NAME = 'img-cache-v1';
+const IMAGES = [
+  '/imagenes/grupo_3.webp',
+  '/imagenes/grupo_en_B_y_N.webp',
+  '/imagenes/integrantes/adriana.webp',
+  '/imagenes/integrantes/yaniela.webp',
+  '/imagenes/integrantes/pedro.webp',
+  '/imagenes/integrantes/christian.webp',
+  '/imagenes/integrantes/jessica.webp',
+  '/imagenes/integrantes/emanuel.webp',
+  '/imagenes/integrantes/angel.webp',
+  '/imagenes/concierto_de_resurreccion/a.webp',
+  '/imagenes/pascua_joven/a.webp',
+  '/imagenes/grabacion_de_amor_de_dias_grises/a.webp',
+  '/imagenes/ajuria/a.webp',
+  '/imagenes/lajas/b.webp',
+  '/imagenes/cinelajas/a.webp',
+  '/imagenes/mariapolis/b.webp',
+  '/imagenes/cruces_2023/a.webp',
+  '/imagenes/la-habana/a.webp',
+  '/imagenes/virgen_de_la_caridad_cartagena/a.webp',
+  '/imagenes/JDJ/a.webp',
+  '/imagenes/asuncion_de_la_virgen_maria/a.webp',
+  '/imagenes/señora_del_carmen/a.webp',
+  '/imagenes/festival_juvenil/a.webp',
+  '/imagenes/boda_yisel/a.webp',
+  '/imagenes/primer_concierto/a.webp',
+  '/imagenes/primer_instrumental/a.webp',
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
+      .then(cache => cache.addAll(IMAGES))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(response => response || fetch(e.request))
-  );
+self.addEventListener('fetch', (event) => {
+  if (IMAGES.some(img => event.request.url.includes(img))) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+  }
 });
